e9383a963575872fae14eef7b2f3a5d1f9bf4a00
Todor Enikov <todor.enikov@fourth.com>
2023-09-25 15:02:23 +0300

    Add project files.

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamoXMLToJSON.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        // POST: FileUploadController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult XMLToJSON(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
